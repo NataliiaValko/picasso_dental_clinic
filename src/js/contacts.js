d46e1bc9b9824ac5156e6dada6ab9bcce8aa5496
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2024-05-03 00:15:43 +0300

    Table-price done
    верстка + стилі на всі ширини екранів, залишиться лише псевдоелементи додати

---
+++ src/js/contacts.js
@@ -0,0 +1,5 @@
+const orderBtn = document.querySelector('.contacts-btn-light');
+const consultBtn = document.querySelector('.contacts-btn-dark');
+
+orderBtn.addEventListener('click', () => {});
+consultBtn.addEventListener('click', () => {});
