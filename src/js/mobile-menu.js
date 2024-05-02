3ca00bf1f149c1bb54cf23b2a280ebf69ba20d55
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2024-05-02 18:34:43 +0200

    Fixing small issues + styles + code prettifying

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,23 +1,30 @@
 (() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
+  const mobileMenu = document.querySelector('.js-mobile-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-mobile-menu');
+  const closeMenuBtn = document.querySelector('.js-close-mobile-menu');
+  const body = document.querySelector('body');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    mobileMenu.classList.toggle('visually-hidden');
+
+    if (!isMenuOpen) {
+      body.style.overflow = 'hidden'; // Блокуємо прокрутку
+    } else {
+      body.style.overflow = ''; // Розблоковуємо прокрутку
+    }
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
-  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  window.matchMedia('(min-width: 1440px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
+    body.style.overflow = ''; // Розблоковуємо прокрутку при зміні розміру вікна
   });
 })();
