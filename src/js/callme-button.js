05003769b625517459e212d3a2f62e0a7221cbad
Yana Lunina <yanalunina96@gmail.com>
2024-05-03 21:52:45 +0300

    update callme button

--- src/js/callme-button.js
+++ src/js/callme-button.js
@@ -1,5 +1,5 @@
 import { openCallModal } from "./modal-call"
 
-const callMeButton = document.getElementById('#call-me-button-modal')
+const callMeButton = document.getElementById('call-me-button-modal')
 
 callMeButton.addEventListener('click', openCallModal)
