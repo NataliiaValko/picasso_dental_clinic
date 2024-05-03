baf422e23a22d2bf86e5c5432505f634b6ced9cc
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2024-05-03 20:52:42 +0300

    додав хендлери для модалок на кнопки

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,5 +1,10 @@
+import { openConsultationModal } from './modal-consultation';
+import { openAppointmentModal } from './modal-appointment';
+
 const orderBtn = document.querySelector('.contacts-btn-light');
 const consultBtn = document.querySelector('.contacts-btn-dark');
 
-orderBtn.addEventListener('click', () => {});
-consultBtn.addEventListener('click', () => {});
+console.log(orderBtn);
+
+orderBtn.addEventListener('click', openAppointmentModal);
+consultBtn.addEventListener('click', openConsultationModal);
